66a6160b8c822ce1e277dce5a0c4b165a9c3cd18
Oleg.Nee <89423263+olegnee@users.noreply.github.com>
2022-07-02 04:33:59 +0900

    second part of homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,54 @@
 package com.company;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         System.out.println("Создаю потоки...");
-        MyRunnable runnable = new MyRunnable();
         ThreadGroup myGroup = new ThreadGroup("main group");
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
 
+        MyThread thread1 = new MyThread( "поток 1", 2000, 5);
+        MyThread thread2 = new MyThread( "поток 2", 2500, 6);
+        MyThread thread3 = new MyThread( "поток 3", 3000, 4);
+        MyThread thread4 = new MyThread( "поток 4", 2700, 6);
 
-        Thread thread1 = new MyThread(myGroup, runnable, "поток 1");
-        Thread thread2 = new MyThread(myGroup, runnable, "поток 2");
-        Thread thread3 = new MyThread(myGroup, runnable, "поток 3");
-        Thread thread4 = new MyThread(myGroup, runnable, "поток 4");
-        Thread thread5 = new ThreadKiller(myGroup);
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.setDaemon(true);
-        thread5.start();
+        Callable <Integer> myCallable1 = new MyThread( "поток 1", 2000, 5);
+        Callable <Integer> myCallable2 = new MyThread( "поток 2", 2500, 6);
+        Callable <Integer> myCallable3 = new MyThread( "поток 3", 3000, 4);
+        Callable <Integer> myCallable4 = new MyThread( "поток 4", 2700, 6);
+        List<MyThread> someTasks = new ArrayList<>();
+        someTasks.add(thread1);
+        someTasks.add(thread2);
+        someTasks.add(thread3);
+        someTasks.add(thread4);
+        Future<Integer> answer1 = threadPool.submit(myCallable1);
+        Future<Integer> answer2 = threadPool.submit(myCallable2);
+        Future<Integer> answer3 = threadPool.submit(myCallable3);
+        Future<Integer> answer4 = threadPool.submit(myCallable4);
+        Integer result1 = answer1.get();
+        Integer result2 = answer2.get();
+        Integer result3 = answer3.get();
+        Integer result4 = answer4.get();
+        System.out.println(result1);
+        System.out.println((result2));
+        System.out.println((result3));
+        System.out.println((result4));
+        Integer anyResult = threadPool.invokeAny(someTasks);
+        threadPool.shutdown();
+        System.out.println(anyResult);
+//        Thread thread5 = new ThreadKiller(myGroup);
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
+//        thread5.setDaemon(true);
+//        thread5.start();
 
     }
 }
--- src/com/company/MyThread.java
+++ src/com/company/MyThread.java
@@ -1,23 +1,49 @@
 package com.company;
 
-import java.util.TimerTask;
+import java.util.concurrent.Callable;
 
-class MyThread extends Thread implements Runnable{
-    public MyThread(ThreadGroup group, Runnable target, String name) {
-        super(group, target, name);
+class MyThread extends Thread implements Callable<Integer> {
+    String name;
+    int intervalTime;
+    int count;
+
+    public MyThread(String name, int time, int count) {
+        this.name = name;
+        this.intervalTime = time;
+        this.count = count;
     }
+//
+//    @Override
+//    public void run() {
+//        try {
+//            while(!isInterrupted()) {
+//                MyThread.sleep(2500);
+//                System.out.printf("Я %s. Всем привет!\n" , getName());
+//            }
+//        } catch (InterruptedException err) {
+//            System.out.println();
+//        } finally{
+//            System.out.printf("%s завершен\n", getName());
+//        }
+//    }
 
     @Override
-    public void run() {
+    public Integer call() throws Exception {
+        int result = 0;
         try {
-            while(!isInterrupted()) {
-                MyThread.sleep(2500);
-                System.out.printf("Я %s. Всем привет!\n" , getName());
+            int i = 0;
+
+            while(!(i == count)) {
+                i++;
+                MyThread.sleep(intervalTime);
+                System.out.printf("Я %s. Всем привет!\n" , name);
+                result++;
             }
         } catch (InterruptedException err) {
             System.out.println();
         } finally{
-            System.out.printf("%s завершен\n", getName());
+            System.out.printf("%s завершен\n", name);
         }
+        return result;
     }
 }
